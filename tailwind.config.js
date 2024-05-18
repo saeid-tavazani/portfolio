67440b70aad9b8c6494514951477b8afa3c27bde
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-18 17:46:10 +0330

    inition config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      darkBlue: "#191E29",
+      lightGold: "#F3D693",
+      lightPink: "#E68895",
+      purple: "#6551D4",
+      lightGrayishBlue: "#919CB5",
+    },
+  },
+  plugins: [],
+};
