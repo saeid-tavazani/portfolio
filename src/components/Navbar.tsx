52c2804d2f79ca11a3a2c93be965d8a9f6018400
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-19 13:22:38 +0330

    fix phone number

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,8 +20,18 @@ const Navbar = () => {
             <a href="#">پروژه ها</a>
           </li>
         </ul>
-        <a className="hidden md:block" href="#">
-          تماس با من
+        <a className="hidden md:flex items-center gap-2" href="tel:09911561251">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+          </svg>
+          <span>تماس با من</span>
         </a>
       </article>
     </header>
