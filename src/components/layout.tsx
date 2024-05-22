95660087a7c982e6bf59264d9999f9a5e56c3e3d
saeid-tavazani <saeidtavazani@gmail.com>
2024-05-22 11:45:02 +0330

    fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,13 +4,16 @@ type prop = {
   children?: ReactNode;
   className?: string;
   slug?: string;
+  display?: "grid" | "flex";
 };
 
-const Layout = ({ children, className, slug }: prop) => {
+const Layout = ({ children, className, slug, display }: prop) => {
   return (
-    <section id={slug} className="w-screen h-screen relative">
+    <section id={slug} className="w-screen h-lvh relative">
       <article
-        className={`container mx-auto grid h-full pt-28 lg:pt-0 items-center px-5 gap-4 ${
+        className={`container mx-auto ${
+          display ? display : "grid"
+        } h-full pt-28 lg:pt-0 items-center px-5 gap-4 ${
           className ? className : ""
         }`}
       >
